2e0ec208d955516f3373c1244748fd90dfda1f15
Lucas Fernando <lucasfernandoa.s97@gmail.com>
2025-03-06 22:25:12 -0300

    att Ex 09 e 010

---
+++ Aula11/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 6.5
+console.log(`a valocidade do seu carro é ${vel} km`)
+if (vel > 60){ //condição simples!
+    console.log('multado')
+}
+console.log('diriga sempre com o cinto de segurança')
---
+++ Aula11/Ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil' 
+console.log(`vivendo no ${pais}`)
+if (pais == 'brasil'){
+    console.log('brasileira')
+} else {
+    console.log('estrageira')
+}
