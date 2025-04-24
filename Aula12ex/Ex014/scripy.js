e08ceffeb67b2a3b3c69b3c223c1a907ffc789e4
Lucas Fernando <lucasfernandoa.s97@gmail.com>
2025-04-24 19:26:13 -0300

    att de atividade

--- Aula12ex/Ex014/scripy.js
+++ Aula12ex/Ex014/scripy.js
@@ -1,1 +1,21 @@
-alert ('ola')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+        document.body.style.background = '#ded8e2'
+    }
+    else if (hora >= 12 && hora < 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#f7c735'
+    }
+    else {
+        img.src = 'noite.png'
+        document.body.style.background = '#392c48'
+    }
+}
