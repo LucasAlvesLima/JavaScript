f735a5311839071ff10c750e4b36fec39a3391e1
Lucas Fernando <lucasfernandoa.s97@gmail.com>
2025-10-30 20:53:33 -0300

    att do propjeto

---
+++ Aula 14Ex/aula16/scripy.js
@@ -0,0 +1,48 @@
+
+
+
+function contar(){
+
+    let fim = document.getElementById("txtf")
+    let ini = document.getElementById("txti")
+    let passo = document.getElementById("txtp")
+    let res = document.getElementById("res")
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert("eRRo, Faltam Dados")
+        res.innerHTML = "impossivel contar!!"
+        
+    }
+    
+    else {
+        res.innerHTML = 'contando... <br>'
+
+        let i = Number (ini.value)
+        let f = Number (fim.value)
+        let p = Number (passo.value)
+
+        if( p <= 0 ){
+            window.alert("Passo invalido ! considerando a PASSO 1")
+            p = 1
+        }
+
+         if( i < f ){
+                 //contagem crescente
+            for(let c = i; c <= f; c += p){
+
+                res.innerHTML  += ` ${c} \u{1f449} `
+            }
+                res.innerHTML += `\u{1f3c1}`
+         } else {
+                // contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+
+                res.innerHTML += ` ${c} \u{1f449} `
+
+            }
+                
+         }
+        
+    }
+        
+}
